59a76023dc227b660e4480a8b642a0109b94ddb5
Daniel <anchorquery@gmail.com>
2024-08-15 21:38:59 -0400

    fdafv

--- lib/pages/events_copy/events_copy_widget.dart
+++ lib/pages/events_copy/events_copy_widget.dart
@@ -356,9 +356,9 @@ class _EventsCopyWidgetState extends State<EventsCopyWidget> {
                                                   ),
                                                 );
                                               },
-                                              carouselController:
-                                                  _model.carouselController ??=
-                                                      CarouselController(),
+                                              carouselController: _model
+                                                      .carouselController ??=
+                                                  CarouselSliderController(),
                                               options: CarouselOptions(
                                                 initialPage: max(
                                                     0,
